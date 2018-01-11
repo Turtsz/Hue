ee70e7af75b288e70def8211424f1ef3c2a5fad6
Turtsz <stevensimione@yahoo.com>
2018-01-11 05:00:34 +0000

    uploading to repo

---
+++ src/huecontroller/HueController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package huecontroller;
+
+import com.philips.lighting.hue.sdk.wrapper.discovery.BridgeDiscovery;
+
+/**
+ *
+ * @author pi
+ */
+public class HueController {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        BridgeDiscovery bridgeDiscovery = new BridgeDiscovery();
+        //..
+    }
+    
+}
